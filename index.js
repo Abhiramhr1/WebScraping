32c5dbf9c92cdfeca44d3b141ff016b84ac5b097
Abhiramhr1 <abhiramhr1.smg@gmail.com>
2024-10-02 11:04:04 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const request = require('request');
+const cheerio = require('cheerio');
+
+
+
+
+
+
+
+app.listen(3000,()=>{
+    console.log('the server is listening at port : 3000');
+})
